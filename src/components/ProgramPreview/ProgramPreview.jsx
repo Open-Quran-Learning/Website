917f14e368c88ec125330ead02af68d263a10f91
hazemgabr232 <hazemgabr232@gmail.com>
2020-04-13 05:23:39 +0200

    modify program perview

--- src/components/ProgramPreview/ProgramPreview.jsx
+++ src/components/ProgramPreview/ProgramPreview.jsx
@@ -1,17 +1,37 @@
-import React, { Component } from 'react';
-import "./previewStyle.css"
+import React, { Component } from "react";
+import {
+  Card,
+  Button,
+  CardHeader,
+  CardFooter,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardImg,
+  Badge,
+  Progress
+} from "reactstrap";
+import "./previewStyle.css";
 
 class ProgramPreview extends Component {
-    state = {  }
-    render() { 
-        return (
-            <div className="programPreview">
-                <img className="col-1 float-right" src="./assets/images/logo.png"></img>
-                <label fo="programPreview">برنامج تاج الكرامة</label>
-                <p type="text" id="programPreview"> .(100%) وصف صغير عن البرنامج , و نسبة المحتوى التي تم اجتيازها</p>
-            </div>
-          );
-    }
+  state = {};
+  render() {
+    return (
+      <div className="col-6 col-sm-2 m-3 programPreview">
+        <Card style={{ width: "125%", height: "100%" }}>
+          <CardBody>
+            <CardImg width="100%" src="./assets/images/logo.png" />
+            <CardHeader tag="h4">اسم البرنامج</CardHeader>
+            <CardText>نبذة بسيطه مختصره رائعة عن الكورس</CardText>
+            <Progress color="success" value="3" max="5" />
+            <CardFooter>
+              <Badge color="success">تعليمى</Badge>
+            </CardFooter>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
 }
- 
+
 export default ProgramPreview;
