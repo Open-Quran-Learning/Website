e86993fd47c0bdea6b16e3a1761885f76126f059
hazemgabr232 <hazemgabr232@gmail.com>
2020-04-13 05:18:58 +0200

    sticky navbar

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -28,7 +28,7 @@ class Header extends Component {
   render() {
     return (
       <>
-        <Navbar dark expand="md">
+        <Navbar dark expand="md" sticky={"top"}>
           <div className="container">
             <NavbarToggler onClick={this.toggleNav} />
             <NavbarBrand className="mr-auto" href="/">
