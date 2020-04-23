0880158419c241782bbb2328fdd58a5cf64be7e6
yossef-khaled <yossef.k.y333@gmail.com>
2020-04-23 23:00:39 +0200

    Add Email validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,7 +4,8 @@ import "./ContactForm.css";
 
 class ContactForm extends Component {
     state = { 
-        topic: "",
+        topic: "برنامج",
+        email: "",
         message: ""
      }
      
@@ -12,11 +13,44 @@ class ContactForm extends Component {
         this.setState({topic: event.target.value})
     }
 
+    handleMailChange = (event) => {
+        this.setState({email: event.target.value})
+    }
+
     handleMessageChange = (event) => {
         this.setState({message: event.target.value})
     }
 
     handleSubmit = (event) => {
+        let msg = {
+            topic: "",
+            email: "",
+            message: ""
+        }
+        const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        expression.test(String(this.state.email).toLowerCase()) ? window.alert(
+        "هل تريد أن ترسل هذه الرسالة ؟",
+        [
+            {
+                text: "أجل",
+                onPress: () => { msg.topic = this.state.topic
+                    msg.topic = this.state.topic
+                    msg.topic = this.state.topic
+                    console.log(msg)
+                    this.setState ({ 
+                        topic: "برنامج",
+                        email: "",
+                        message: ""
+                     })
+                }
+            },
+            {
+                text: "لا",
+                onPress: () => ""
+            }
+        ]
+         )  
+        :window.alert("أعد كتابة بريدك الإلكتروني") 
         console.log(this.state)
         event.preventDefault()
     }
@@ -35,6 +69,10 @@ class ContactForm extends Component {
                     </select>
                 </div>
                 <br/>
+                <div className="emailForm">
+                    <label className="enterMail" for="userMail">ادخل بريدك الإلكتروني</label>
+                    <input id="userMail" type="Email" onChange={this.handleMailChange} placeholder="ادخل بريدك الإلكتروني"></input>
+                </div>
                 <br/>
                 <hr/>
                 <div className="messageBox"> 
