54981a4ee4392a8003848e621d29f95f8e66ec9d
Karim_emad <karimemad167@gmail.com>
2020-04-23 06:04:54 +0200

    course page finally

--- src/components/CourseDetails/CourseDetails.jsx
+++ src/components/CourseDetails/CourseDetails.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
-
+import "./CourseDetails.css";
 class CourseDetails extends Component {
   render() {
     const { items } = this.props;
     const theItems = items.map((item) => {
       return (
-        <div>
+        <div className="course">
           <h2>{item.name}</h2>
           <p>{item.description}</p>
+          <button>الدخول الي الدرس</button>
         </div>
       );
     });
-    return <div>{theItems}</div>;
+    return (
+      <div>
+        <div className="header">
+          <h2>{this.props.name}</h2>
+        </div>
+        <div className="container">{theItems}</div>
+      </div>
+    );
   }
 }
 
