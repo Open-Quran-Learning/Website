6d4ff43c9083e508f4c8ded9498c53565775444e
yossef-khaled <yossef.k.y333@gmail.com>
2020-04-23 07:05:41 +0200

    ContactForm component

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import "./ContactForm.css";
+
+
+class ContactForm extends Component {
+    state = { 
+        topic: "",
+        message: ""
+     }
+     
+    handleListChange = (event) => {
+        this.setState({topic: event.target.value})
+    }
+
+    handleMessageChange = (event) => {
+        this.setState({message: event.target.value})
+    }
+
+    handleSubmit = (event) => {
+        console.log(this.state)
+        event.preventDefault()
+    }
+
+    render() { 
+        return (  
+            <React.Fragment>
+                <label for="messageList" className="listHeader">موضوع الرسالة  </label>
+                <select id="messageList" className="messageList" onChange={this.handleListChange}>
+                    <option value="برنامج">برنامج</option>
+                    <option value="كورس">كورس</option>
+                    <option value="درس">درس</option>
+                    <option value="معلم">معلم</option>
+                    <option value="أخرى">أخرى</option>
+                </select>
+                <br/>
+                <br/>
+                <hr/>
+                <div className="messageBox"> 
+                    <h3 for="userMessage">اكتب رسالتك هنا</h3>
+                    <textarea id="userMessage" onChange={this.handleMessageChange} rows="8" cols="60"/>
+                </div>
+                <br/>
+                <div className="center">
+                    <button className="btn btn-success sm m-2 submitButton" onClick={this.handleSubmit}>إرسال</button>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default ContactForm;
