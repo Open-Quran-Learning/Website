1bb1aa8ab9d91a8b8c0bce1a2630d374701a03dd
AbdelrahmanElsemary <abdelrahman_semary96@yahoo.com>
2020-04-29 23:01:46 +0200

    wiring login api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from "./components/FooterComponent";
 import Home from "./components/HomeComponent/HomeComponent";
 import ProgramDetails from "./components/programdetails/ProgramDetails";
 import StudentProfilePage from "./components/StudentProfilePage/StudentProfilePage";
+import Login from "./components/Login/LoginComponent";
+import Registration from "./components/Login/RegistrationComponent";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 class App extends Component {
@@ -27,6 +29,8 @@ class App extends Component {
             <Route path="/home" component={this.HomePage} />
             <Route path="/program" component={() => <ProgramDetails />} />
             <Route path="/student" component={() => <StudentProfilePage />} />
+            <Route path="/Login" component={() => <Login />} />
+            <Route path="/Registration" component={() => <Registration />} />
 
             <Redirect to="/home" />
           </Switch>
