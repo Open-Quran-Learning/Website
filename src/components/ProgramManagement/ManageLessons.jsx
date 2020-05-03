ff35ed4225f2362fbca9925b21e28ef257ecf82d
mhashim6 <m6hashim@gmail.com>
2020-05-04 01:33:29 +0200

    added lessons managment component

---
+++ src/components/ProgramManagement/ManageLessons.jsx
@@ -0,0 +1,193 @@
+import React, { useState, Fragment as div } from "react";
+import "./ManageLessons.scss";
+import { YouTubeThmbnailURL } from "./utils";
+import { useEffect } from "react";
+
+// TODO: fetch existing lessons in this course for intial state.
+const ManageLessons = () => {
+  const [lessons, updateLessons] = useState([]);
+
+  const updateOneLesson = (lesson, index) => {
+    console.log(lesson);
+    const current = [...lessons];
+    current[index] = { ...current[index], ...lesson };
+    updateLessons(current);
+  };
+
+  const addLesson = (lesson) => updateLessons([...lessons, lesson]);
+  const removeLesson = (index) => {
+    const current = [...lessons];
+    current.splice(index, 1);
+    console.log(current);
+    updateLessons(current);
+  };
+
+  return (
+    <div className="manageLessons">
+      {lessons.map((lesson, i) => (
+        <div key={i}>
+          <ManageLessonEdit
+            videoUrl={lesson.videoUrl}
+            article={lesson.article}
+            references={lesson.references}
+            index={i}
+            update={(newLesson) => {
+              updateOneLesson(newLesson, i);
+            }}
+          />
+          {/* <div className="separator"></div> */}
+        </div>
+      ))}
+      <div className="plusMinusBtns">
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={() => {
+            addLesson({
+              videoUrl: "",
+              article: "",
+              references: [],
+            });
+          }}
+        >
+          +
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={() => {
+            removeLesson(lessons.length - 1);
+          }}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const ManageLessonEdit = ({ videoUrl, article, references, index, update }) => {
+  const [content, updateContent] = useState({
+    videoUrl: videoUrl,
+    article: article,
+    references: references,
+  });
+
+  const videoInputId = `yt_input_${index}`;
+  const articleInputId = `article_input_${index}`;
+
+  return (
+    <div className="lessonContent">
+      <div className="videoPreview">
+        <input
+          id={videoInputId}
+          className="ytVideoInput form-control"
+          type="text"
+          placeholder={"رابط الڤيديو"}
+          value={content.videoUrl}
+          onInput={(e) => {
+            const url = e.target.value;
+            updateContent({ ...content, videoUrl: url });
+            update(content);
+          }}
+        />
+        <img src={YouTubeThmbnailURL(content.videoUrl)} />
+      </div>
+      <div className="articleInput">
+        <textarea
+          value={content.article}
+          placeholder={"اكتب مقال"}
+          id={articleInputId}
+          class="form-control"
+          rows="8"
+        ></textarea>
+      </div>
+      <ManageReferences
+        initialReferences={references}
+        update={(references) =>
+          updateContent({ ...content, references: references })
+        }
+      />
+    </div>
+  );
+};
+
+const ManageReferences = ({ initialReferences, update }) => {
+  const [references, updateReferences] = useState([...initialReferences]);
+
+  const updateOneReference = (reference, index) => {
+    console.debug(reference);
+    const current = [...references];
+    current[index] = { ...current[index], ...reference };
+    updateReferences(current);
+  };
+
+  const addReference = (reference) =>
+    updateReferences([...references, reference]);
+  const removeReference = (index) => {
+    const current = [...references];
+    current.splice(index, 1);
+    console.log(current);
+    updateReferences(current);
+  };
+
+  useEffect(() => {
+    update(references);
+  }, [references]);
+
+  return (
+    <div className="manageReferences">
+      <label>المراجع</label>
+
+      {references.map((r, i) => (
+        <div className="referencesInputs" key={i}>
+          <input
+            className="refTitleInput form-control"
+            type="text"
+            placeholder={"اسم المرجع"}
+            value={references[i].title}
+            onInput={(e) => {
+              updateOneReference({ ...r, title: e.target.value }, i);
+            }}
+          />
+          <input
+            className="refUrlInput form-control"
+            type="text"
+            placeholder={"رابط المرجع"}
+            value={references[i].url}
+            onInput={(e) => {
+              updateOneReference({ ...r, url: e.target.value }, i);
+            }}
+          />
+        </div>
+      ))}
+      <div className="plusMinusBtns">
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={() => {
+            addReference({
+              title: "",
+              url: "",
+            });
+          }}
+        >
+          +
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={() => {
+            removeReference(references.length - 1);
+          }}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ManageLessons;
