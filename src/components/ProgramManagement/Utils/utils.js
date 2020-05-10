289ec37b19d5eb4671c33336310d58d269b88e28
mhashim6 <m6hashim@gmail.com>
2020-05-10 09:30:34 +0200

    added validation utils

--- src/components/ProgramManagement/Utils/utils.js
+++ src/components/ProgramManagement/Utils/utils.js
@@ -2,7 +2,7 @@
 const YT_REGEX = /(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)/;
 
 export const YouTubeThmbnailURL = (YT_URL) => {
-    console.log(`https://img.youtube.com/vi/${YouTubeID(YT_URL)}/hqdefault.jpg`)
+    console.debug(`https://img.youtube.com/vi/${YouTubeID(YT_URL)}/hqdefault.jpg`)
     return `https://img.youtube.com/vi/${YouTubeID(YT_URL)}/hqdefault.jpg`;
 };
 
@@ -10,3 +10,7 @@ export const YouTubeID = (YT_URL) => {
     const matches = YT_URL.match(YT_REGEX)
     return matches ? matches[5] : "unknown";
 };
+
+export const isBlankObject = (obj) => Object.values(obj).filter((it) => it != undefined && it != null && it != "").length == 0;
+
+export const userIsSure = (msg = "هل أنت متأكد؟") => window.confirm(msg); 
